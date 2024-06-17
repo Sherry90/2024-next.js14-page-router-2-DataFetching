8ef3ef9afbc6e93b98a68035b33bab81efbb6301
Sherry90 <hhlsdkm@gmail.com>
2024-06-17 22:26:32 +0900

    feat: read file, return props

--- pages/[pid].js
+++ pages/[pid].js
@@ -11,6 +11,18 @@ export const getStaticProps = async (context) => {
   const { params } = context;
 
   const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath, "utf-8");
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
 };
 
 export default ProductDetailPage;
