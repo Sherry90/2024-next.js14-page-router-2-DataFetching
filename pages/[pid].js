3352320b751c68204ebf9663027fab3c4df00b00
Sherry90 <hhlsdkm@gmail.com>
2024-06-23 19:49:36 +0900

    refactor: fs read file getData()

--- pages/[pid].js
+++ pages/[pid].js
@@ -16,14 +16,18 @@ const ProductDetailPage = (props) => {
   );
 };
 
+const getData = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath, "utf-8");
+  return JSON.parse(jsonData);
+};
+
 export const getStaticProps = async (context) => {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath, "utf-8");
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
